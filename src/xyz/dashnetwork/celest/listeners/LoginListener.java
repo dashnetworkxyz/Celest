410239c8310c5c9142b48f7f78afce2bd2cd19fb
Your Name <you@example.com>
2022-07-22 12:16:08 -0600

    Update Copyright notice

--- src/xyz/dashnetwork/celest/listeners/LoginListener.java
+++ src/xyz/dashnetwork/celest/listeners/LoginListener.java
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2022. Andrew Bell.
- * All rights reserved.
+ * Copyright (C) 2022 Andrew Bell. - All Rights Reserved
+ *
+ * Unauthorized copying or redistribution of this file in source and binary forms via any medium
+ * is strictly prohibited.
  */
 
 package xyz.dashnetwork.celest.listeners;
