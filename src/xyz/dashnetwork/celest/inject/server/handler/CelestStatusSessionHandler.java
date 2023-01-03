b7033b44949eabcf993de36a50e3806d8fa8a2bd
MasterDash5 <andrewbell25@icloud.com>
2023-01-02 20:03:58 -0700

    License under GNU GPL-v2.0

--- src/xyz/dashnetwork/celest/inject/server/handler/CelestStatusSessionHandler.java
+++ src/xyz/dashnetwork/celest/inject/server/handler/CelestStatusSessionHandler.java
@@ -1,8 +1,18 @@
 /*
- * Copyright (C) 2022 Andrew Bell - All Rights Reserved
+ * Celest
+ * Copyright (C) 2023  DashNetwork
  *
- * Unauthorized copying or redistribution of this file in source and binary forms via any medium
- * is strictly prohibited.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package xyz.dashnetwork.celest.inject.server.handler;
