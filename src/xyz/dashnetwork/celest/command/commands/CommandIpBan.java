9cfb7692291eb555633730ed43ac5377872a1b57
MasterDash5 <andrewbell25@icloud.com>
2023-05-03 09:55:52 -0600

    color fix

--- src/xyz/dashnetwork/celest/command/commands/CommandIpBan.java
+++ src/xyz/dashnetwork/celest/command/commands/CommandIpBan.java
@@ -78,7 +78,7 @@ public final class CommandIpBan extends CelestCommand {
         }
 
         builder = new MessageBuilder();
-        builder.append("&6&l»&7 " + address.getString() + " permanently banned by ");
+        builder.append("&6&l»&6 " + address.getString() + " permanently banned by ");
         builder.append(new NamedSourceFormat(named));
         builder.append("&7.");
         builder.append("\n&6&l»&7 Hover for details.")
--- src/xyz/dashnetwork/celest/command/commands/CommandIpMute.java
+++ src/xyz/dashnetwork/celest/command/commands/CommandIpMute.java
@@ -77,7 +77,7 @@ public final class CommandIpMute extends CelestCommand {
         }
 
         builder = new MessageBuilder();
-        builder.append("&6&l»&7 " + address.getString() + " permanently muted by ");
+        builder.append("&6&l»&6 " + address.getString() + " permanently muted by ");
         builder.append(new NamedSourceFormat(named));
         builder.append("&7.");
         builder.append("\n&6&l»&7 Hover for details.")
