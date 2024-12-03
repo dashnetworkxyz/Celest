9519c0f7f03ebef727cdedcc69ad6d0f59853581
masterdash5 <constant4337@molecularmail.com>
2024-12-02 23:37:25 -0700

    match with http

--- src/xyz/dashnetwork/celest/utils/StringUtils.java
+++ src/xyz/dashnetwork/celest/utils/StringUtils.java
@@ -51,7 +51,7 @@ public final class StringUtils {
     }
 
     public static boolean matchesUrl(@NotNull String string) {
-        return string.matches("(?:(https?)://)?([-\\w_.]{2,}\\.[a-z]{2,4})(/\\S*)?");
+        return string.matches("(?:(http(s?)?)://)?([-\\w_.]{2,}\\.[a-z]{2,4})(/\\S*)?");
     }
 
     public static boolean matchesIp(@NotNull String string) {
